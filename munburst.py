3929284aa6512819e67f2bbe54cb4a56aae58650
Jan Fousek <izaak@mail.muni.cz>
2017-11-09 20:26:07 +0100

    fixed an import

--- munburst.py
+++ munburst.py
@@ -11,7 +11,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 import matplotlib.transforms as transforms
 import textwrap
